4b3de741accec8e0eaa4b864392aa15d921ae54d
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-03-27 11:05:48 +0600

    added nav

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg'
+const Header = () => {
+        return (
+           <nav className='header'>
+                    <img src={logo} alt="" />  
+                 <div className='menu'>
+                <a href="/1">Shop</a>
+                <a href="/2">Order</a>
+                <a href="/3">Inventory</a>
+                <a href="/4">Login</a>
+                 </div>  
+           </nav>
+        );
+};
+
+export default Header;
