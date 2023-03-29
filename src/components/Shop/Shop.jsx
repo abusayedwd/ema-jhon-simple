e0d3e3f36ed589541bdace077561606c6459b5e3
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-03-29 15:48:18 +0600

    added product

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Product from '../Products/Product';
 import Cart from '../Cart/Cart';
 import './Shop.css'
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
         const [products, setProducts] = useState([]);
@@ -11,9 +12,38 @@ const Shop = () => {
                 .then(res => res.json())
                 .then(data => setProducts(data))
         },[])
+        useEffect(() => {
+                console.log('product',products)
+                const storedCart = getShoppingCart();
+                let saveCart = [];
+                for(const id in storedCart){
+                const addedProduct = products.find(product => product.id === id);
+                if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity
+                // console.log(addedProduct) 
+                saveCart.push(addedProduct)
+                }
+                }
+              setCart(saveCart)   
+                
+        }, [products])
         const addToCart = (product) => {
-                 const newCart = [...cart,product]
+                //  const newCart = [...cart,product]
+                let newCart = [];
+                const exists = cart.find(pd => pd.id === product.id);
+                if(!exists){
+                    product.quantity = 1;
+                    newCart = [...cart, product];
+
+        }
+        else{
+                exists.quantity = exists.quantity + 1;
+                const remaining = cart.filter(pd => pd.id !==product.id);
+                newCart = [...remaining, exists];
+        }
                  setCart(newCart)
+                 addToDb(product.id)
         }
         return (
                 <div className='shop-container'>
